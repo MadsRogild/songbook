ea61d3ca1b4f53519fd3a989bc114a0c46dfcb1f
MadsRogild <Mads.Rogild@gmail.com>
2016-07-11 17:49:17 +0200

    songbook, only works with python 2.x.x

--- preamble.py
+++ preamble.py
@@ -113,8 +113,6 @@ def create_preamble(unf, camp, name, style, logo, empty):
                 f.write("""\\mbox{\\includegraphics[scale="""+str(scale)+"""]{"""+logo+"""}}\n""")
             else:
                 f.write("""\\mbox{\\includegraphics[scale="""+str(scale)+"""]{"""+logo+"""}}\n""")
-        else:
-            f.write("""\\includegraphics[]{"""+logo+"""}}\n""")
         f.write("""\\vspace{1cm}
 \\begin{center}\n""")
         if camp:
